acce73ffbfffe6bacffe58cbf4030d255695a2f8
Mahika Chaurasiya <mahikachaurasiya2006@gmail.com>
2026-06-04 16:18:08 +0530

    url added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,15 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+ app.use(
+  cors({
+    origin: [
+      "http://localhost:8080",
+      "https://leadflow-crm-red.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
