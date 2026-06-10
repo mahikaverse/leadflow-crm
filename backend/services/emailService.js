ca7267d226a8e57e3bda85d6e0badc9a200a30ef
Mahika <team.workholicagency@gmail.com>
2026-06-10 16:54:29 +0530

    resend added

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,56 +1,32 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-transporter.verify((error) => {
-  if (error) {
-    console.error("SMTP VERIFY ERROR:", error);
-  } else {
-    console.log("SMTP SERVER READY");
-  }
-});
+const resend = new Resend(
+  process.env.RESEND_API_KEY
+);
 
 async function sendVerificationEmail(email, token) {
-  const verificationLink =
-    `${process.env.FRONTEND_URL}/verify-email/${token}`;
 
-  console.log("Sending email with:");
-  console.log("USER:", process.env.EMAIL_USER);
-  console.log("PASS EXISTS:", !!process.env.EMAIL_PASS);
+ const verificationLink =
+  `https://leadflow-crm-hm7v.onrender.com/api/verify-email/${token}`;
 
-  console.log("STARTING EMAIL SEND");
+  const result = await resend.emails.send({
+    from: "LeadFlow <onboarding@resend.dev>",
+    to: email,
+    subject: "Verify Your Email",
+    html: `
+      <h2>Email Verification</h2>
 
-  try {
-    const result = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Verify Your Email",
-      html: `
-        <h2>Email Verification</h2>
-        <a href="${verificationLink}">
-          Verify Email
-        </a>
-      `,
-    });
+      <p>
+        Click below to verify your email
+      </p>
 
-    console.log("EMAIL SENT SUCCESS");
-    console.log(result);
-
-    return result;
-
-  } catch (error) {
-    console.error("EMAIL SEND ERROR:");
-    console.error(error);
-    throw error;
-  }
+      <a href="${verificationLink}">
+        Verify Email
+      </a>
+    `,
+  });
+console.log("RESEND RESPONSE:");
+  console.log(result);
 }
 
 module.exports = {
